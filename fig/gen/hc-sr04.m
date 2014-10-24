47b65c370d978f84bac21bc2e8f039cc95a03045
Nino Ninux <nino.ninux@gmail.com>
2014-10-24 19:26:47 +0200

    Weitere Plots erzeugt

--- fig/gen/hc-sr04.m
+++ fig/gen/hc-sr04.m
@@ -2,6 +2,10 @@ s = [50:10:170]'
 t_mean = ([2.987, 3.503, 4.060, 4.766, 5.230, 5.807, 6.413, 7.040, 7.722, 8.229, 8.854, 9.500, 10.06]')
 t_stdev = ([2.4, 2.4, 10, 24, 10, 11, 13, 16, 22, 16, 15, 43, 22]')
 
+angle = ([0:5:40]')*(2*pi/360);
+sense = [180, 123, 120, 119, 113, 106, 104, 77, 0]';
+sense_mod = [180, 123, 120, 119, 0, 0, 0, 0, 0]';
+
 graphics_toolkit("gnuplot")
 
 plot(s, t_mean)
@@ -16,3 +20,11 @@ xlabel("Distanz [cm]")
 ylabel("Jitter [us]")
 print("hc-sr04_jitter.svg", "-dsvg", "-F:16")
 
+polar(angle, sense)
+hold on
+polar(angle, sense_mod, "r")
+title("Messbereich")
+xlabel("Distanz [cm]")
+legend(" normal ", " modifiziert ")
+print("hc-sr04_range.svg", "-dsvg", "-F:8")
+hold off
--- fig/gen/svg2pdf.sh
+++ fig/gen/svg2pdf.sh
@@ -1,6 +1,7 @@
 # convert the SVG files to PDF format
 inkscape -f hc-sr04_accuracy.svg -A hc-sr04_accuracy.pdf
 inkscape -f hc-sr04_jitter.svg -A hc-sr04_jitter.pdf
+inkscape -f hc-sr04_range.svg -A hc-sr04_range.pdf
 
 # move the PDF files to the figure directory "fig"
 mv *.pdf ../.
